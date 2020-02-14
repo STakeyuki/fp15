b8f318a064816241f13f55b449852416fcfa2d5c
takeyuki <stake404@gmail.com>
2020-02-14 13:25:37 +0900

    reset

--- vector.c
+++ vector.c
@@ -2,6 +2,16 @@
 #include "vector.h"
 #include "math.h"
 
+struct color r2color(double r){
+    
+}
+//void showvector(struct vector v, struct coord xy){
+//   double r = sqrt(pow(v.x, 2) + pow(v.y, 2)); 
+//   struct color c = r2color(r); //struct color r2color(double r) 
+//   struct coord s  = {xy.x + VECTORSIZE * v.x / (r * 2), xy.y + VECTORSIZE * v.y /(r * 2) };
+//   struct coord e  = {xy.x - VECTORSIZE * v.x / (r * 2), xy.y - VECTORSIZE * v.y / (r * 2)};//cast??
+//   drawline(s, e, c);
+//}
 void showvector(struct vector v, struct coord xy){
    double r = sqrt(pow(v.x, 2) + pow(v.y, 2)); 
    struct color c = r2color(r); //struct color r2color(double r) 
